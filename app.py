c252f2db5233b42df6284e330299e21dc7709ba3
mari-avp <ACKRONNE@GMAIL.COM>
2025-02-19 21:00:46 -0400

    ✨ FEAT: Recover Password

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from src.database.db import db
 from sqlalchemy import create_engine
+from flask_mail import Mail
 
 # Objetos de las rutas
 from src.routes.index import ind
@@ -35,3 +36,21 @@ db.init_app(app)
 app.register_blueprint(ind)
 app.register_blueprint(pac)
 app.register_blueprint(esp)
+
+
+mail = os.environ['MAIL_USERNAME']
+password = os.environ['MAIL_PASSWORD']
+default_sender = os.environ['MAIL_DEFAULT_SENDER']
+
+# Configuraci�n del correo
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = mail
+app.config['MAIL_PASSWORD'] = password
+app.config['MAIL_DEFAULT_SENDER'] = default_sender
+
+mail = Mail(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/routes/index.py
+++ src/routes/index.py
@@ -1,13 +1,33 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
-from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from werkzeug.security import check_password_hash, generate_password_hash
 from src.database.db import db
-
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadTimeSignature
+from flask_mail import Mail, Message
+from flask import current_app as app
 
 # Entidades
 from src.models.models import Paciente, Especialista, Alimento, Comida, AC
 # //
 
 ind = Blueprint('index', __name__)
+
+# Configuración del correo
+def send_reset_email(user, email):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    token = s.dumps(email, salt='email-confirm')
+    msg = Message('Password Reset Request', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[email])
+    link = url_for('index.reset_token', token=token, _external=True)
+    msg.body = f'''To reset your password, visit the following link:
+{link}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail = Mail(app)
+    try:
+        mail.send(msg)
+        app.logger.info(f'Password reset email sent to {email}')
+    except Exception as e:
+        app.logger.error(f'Failed to send email to {email}: {e}')
 
 # Pagina principal <
 @ind.route('/')
@@ -31,12 +51,6 @@ def login():
         # Check if the user exists in the database
         account = db.session.query(Paciente.id_paciente, Paciente.clave).filter(Paciente.correo == _correo).first()
         
-        # if account and check_password_hash(account.clave, _clave):
-        #     session['logueado'] = True
-        #     session['id'] = account.id_paciente
-        #     _id = session['id']
-            
-        #     return redirect(url_for('paciente.inicio', id=_id))
         if account: # Temporal para hacer pruebas sin seguridad
             session['logueado'] = True
             session['id'] = account.id_paciente
@@ -48,11 +62,6 @@ def login():
         else:    
             account = db.session.query(Especialista.id_espe, Especialista.clave).filter(Especialista.correo == _correo).first()
 
-            # if account and check_password_hash(account.clave, _clave):
-            #     session['logueado'] = True
-            #     session['id'] = account.id_empleado
-            #     _id = session['id']
-            #     return redirect(url_for('especialista.inicio', id=_id))
             if account: # Temporal para hacer pruebas sin seguridad
                 session['logueado'] = True
                 session['id'] = account.id_espe
@@ -83,6 +92,48 @@ def logout(id):
         return "Error 404, No se pudo cerrar la sesion"
 # // >
 
+# Recuperar contraseña <
+@ind.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = db.session.query(Paciente).filter_by(correo=email).first()
+        if user:
+            send_reset_email(user, email)
+            flash('An email has been sent with instructions to reset your password.', 'info')
+            return redirect(url_for('index.login'))
+        else:
+            flash('Email not found', 'danger')
+    return render_template('reset_request.html')
 
-# TODO: Falta recuperar contraseña
+@ind.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+    except (SignatureExpired, BadTimeSignature):
+        flash('The token is invalid or has expired', 'warning')
+        return redirect(url_for('index.reset_request'))
 
+    if request.method == 'POST':
+        password = request.form['password']
+        user = db.session.query(Paciente).filter_by(correo=email).first()
+        if user:
+            user.clave = generate_password_hash(password)
+            db.session.commit()
+            flash('Your password has been updated!', 'success')
+            return redirect(url_for('index.login'))
+    return render_template('reset_token.html')
+# // >
+
+# Test email route
+@ind.route('/test_email')
+def test_email():
+    msg = Message('Test Email', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=['your_email@gmail.com'])
+    msg.body = 'This is a test email.'
+    mail = Mail(app)
+    try:
+        mail.send(msg)
+        return 'Test email sent successfully!'
+    except Exception as e:
+        return f'Failed to send test email: {e}'
